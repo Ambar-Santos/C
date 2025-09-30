6d5f69a151ad9563f2addb6e90efe02c6b6e89cf
Ambar Santos <santosambar738@gmail.com>
2025-09-30 17:56:47 -0400

    Create stdio.c

---
+++ DrChuck_Course/Chapter 1/stdio.c
@@ -0,0 +1,19 @@
+//putchar function
+
+#include <stdio.h>
+
+int main()
+{
+  putchar('E');
+  putchar('X');
+  putchar('C');
+  putchar('A');
+  putchar('L');
+  putchar('I');
+  putchar('B');
+  putchar('U');
+  putchar('R');
+  putchar('\n');
+
+    return 0;
+}
