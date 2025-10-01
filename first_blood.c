06405317911787f97d6793c92d7c63338780fb98
Ambar Santos <santosambar738@gmail.com>
2025-10-01 15:59:10 -0400

    Changes in first_blood.c code

--- first_blood.c
+++ first_blood.c
@@ -1,27 +1,39 @@
+//Program: New line counter
+//New features
+/* - Counting characters
+ * - Couting words 
+ * - A menu 
+ * - A way to let the user decide if continue or not(?)
+ */
+
 #include <stdio.h>
 
 int main (){
+  //Variables
+    int c, nl,nc,nw ; 
+
+  nl = 0; //Ne line, new character and new word counter
+  nc = 0;
+  nw = 0;
   
-    int c, nl; 
-
-    nl = 0;
-
     while ((c = getchar()) != EOF){
 
 	    if(c == '\n')
 	    {
 	       ++nl;
 
-  	    }
+  	    }else if(c != ' ' && c != '\t' ) {
+               ++nc;
+	    }
 
        	    
+       
 
-    }
-
-       printf("%d\n", nl);
-
-
-
+    } 
+  
+   putchar('\n');
+    printf("There are %d new line characters \n", nl);
+       printf("There are %d characters (letters, numbers, punctuation)\n" , nc);
 
   return 0; 	
 }
