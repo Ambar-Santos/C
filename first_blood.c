4443507d692e44f019a0dd0751d735f55077715a
Ambar Santos <santosambar738@gmail.com>
2025-10-02 12:22:56 -0400

    Update in the first_blood.c file

--- first_blood.c
+++ first_blood.c
@@ -1,39 +1,67 @@
-//Program: New line counter
-//New features
-/* - Counting characters
- * - Couting words 
- * - A menu 
- * - A way to let the user decide if continue or not(?)
+/*
+ * Program: Counting Program
+ * Description:
+ *   This program reads characters from standard input until EOF (Ctrl+D on Linux/Mac,
+ *   Ctrl+Z on Windows). It counts:
+ *     - Newline characters
+ *     - Spaces
+ *     - Tabs
+ *     - Non-whitespace characters
+ *     - Words (defined as sequences of non-whitespace characters)
+ *
+ * Usage:
+ *   Run the program, type text, and finish input with EOF.
  */
 
 #include <stdio.h>
 
-int main (){
-  //Variables
-    int c, nl,nc,nw ; 
+int main (){   
+  int c = 0; 
+  int nl = 0; 
+  int nc = 0;
+  int blanks = 0;
+  int tabs = 0;
+  int words = 0;
+  int in_word = 0; 
 
-  nl = 0; //Ne line, new character and new word counter
-  nc = 0;
-  nw = 0;
+
+  //Print Program Title and instructions
+  printf("=== Counting Program ===\n");
+  printf("This program counts: \n");
+  printf(" - Characters\n");
+  printf(" - Spaces\n");
+  printf(" - Tabs\n");
+  printf(" - New line Characters('\\n')\n");
+  printf(" - Words\n"); 
+  printf("Type text and finish with EOF (Ctrl+D or Ctrl+Z).\n\n");
   
     while ((c = getchar()) != EOF){
-
 	    if(c == '\n')
 	    {
 	       ++nl;
+         in_word = 0;
+  	    }else if(c == ' '){
+               ++blanks;
+               in_word = 0;
+	      }else if(c == '\t'){
+               ++tabs;
+               in_word = 0;
+	      }else{
+	         ++nc;
+           if (in_word == 0){
+            ++words; 
+            in_word = 1; 
+           }
+	      }
+ }
 
-  	    }else if(c != ' ' && c != '\t' ) {
-               ++nc;
-	    }
-
-       	    
-       
-
-    } 
   
-   putchar('\n');
+    printf("\n=== Results ===\n");
     printf("There are %d new line characters \n", nl);
     printf("There are %d characters (letters, numbers, punctuation)\n" , nc);
+    printf("There are %d blank spaces \n", blanks);
+    printf("There are %d tabs \n", tabs);
+    printf("There are %d words \n", words);
 
   return 0; 	
 }
