505311f50148b61355582cfef565c377a8b104e4
Ambar Santos <santosambar738@gmail.com>
2025-10-12 19:33:29 -0400

    Create number.c

---
+++ C_Learning/number.c
@@ -0,0 +1,23 @@
+//Directivas del preprocesador y variables 
+//Variable: Posicion de memoria donde se puede asignar un dato
+
+#include <stdio.h>//Libreria
+
+#define PI 3.1416 //Macro 
+
+
+int y = 5; //Variable Global
+
+int main(){
+  
+    int fav_num = 0; //Users favorite Integer Number
+    printf("Type your favorite integer number\n");
+    scanf("%d", &fav_num);
+
+    printf("I hate %d  man, it just sucks!! \n", fav_num);
+    
+    printf("Isn't %d just better?\n", y);
+
+
+    return 0;
+}S
