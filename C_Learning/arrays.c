c0b0ba62df1c06e2036370a778f7ba62234e69f4
Ambar Santos <santosambar738@gmail.com>
2025-10-13 14:08:36 -0400

    Update arrays.c

--- C_Learning/arrays.c
+++ C_Learning/arrays.c
@@ -2,15 +2,33 @@
 # include <stdio.h>
 
 
-int main(int argc, const char *argv)
-{
+int max(int array[], int n){
+    int max_num = 0;
 
+   
+  //Sort trough the array and replace the bigger number
+    for(int i = 0; i < n; i++){
+        
+        if(array[i] > max_num)
+        {
+            max_num = array[i];
+        }
+     
+    }
+    
+    printf("The biggest number in the array is: %d", max_num);
+    return 0;
+}
+
+int main()
+{
+  
       int array[10];
 
-      for(int i = 0; i <= 9; i++){
+      for(int i = 0; i < 10; i++){
         array[i] = i + 1;
-        printf("Number: %d\n", array[i]);
+        printf("The current number is: %d\n", array[i]);
       }
+  max(array, 10);
 
-      
 }
