54c9211b73d717d9fb269e049121a92f4b697970
Ambar Santos <santosambar738@gmail.com>
2025-10-13 12:18:43 -0400

    Update flow.c

--- C_Learning/flow.c
+++ C_Learning/flow.c
@@ -14,10 +14,36 @@ int main(int argc, const char *argv[])
 {
     printf("*** Welcome to my program! ***\n");
   
+    printf("Write a number, any integer: \n");
+    int x = 0;
+    scanf("%d", &x);
 
-    int x = 4;
+    int number = x;
+    if (number == 100){
+        printf("I like that number! \n");
+    }
+
     x = halveIt(x);
-    printf("Half of %d\n", x);
+     
+
+    printf("Half of %d is %d\n",number,  x);
+    printf("Now lets count from 1 to %d\n", number);
+
+    for(int i = 1; i <= number; i++){
+        printf("%d\n", i);
+        printf("                  \n");
+    }
+
+    printf("And now lets count up to %d\n", x );
+    printf("                  \n");
+    
+
+      for(int i = 1; i <= x; i++){
+        printf("%d\n", i);
+        printf("                  \n");
+    }
+
+    getchar();
 
     return 0;
 }
