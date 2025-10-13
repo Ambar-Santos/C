be2a5975f0f0898e4460e3706ecbd4f3246e7635
Ambar Santos <santosambar738@gmail.com>
2025-10-13 11:54:03 -0400

    Update main.c

--- C_Learning/main.c
+++ C_Learning/main.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 
+void doSomethingWithOperators()
+{
+   int x = 2, y = 5;
+
+int z = x + y;
+int num = 8 + z;
+x -= y;
+
+x = 0;
+x = !x;
 
 
-void explainVariables()
-{}
+
+   printf("Result is %d \n", x);
+}
+
 
 int main(int argc, const char * argv[])
 {
-    explainVariables();
+    doSomethingWithOperators();
     printf("Im the best \n");
-    
+    getchar(); //Waits for user to hit enter before quitting
+
     return 0; 
 } //Curly braces code are blocks 
 
-//Statement : anything that ends with a semicolon
