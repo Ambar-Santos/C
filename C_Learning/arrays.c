bf76424d52b4ad9db7c814c6a85bdca93d5108a5
Ambar Santos <santosambar738@gmail.com>
2025-10-13 13:20:04 -0400

    Update arrays.c

--- C_Learning/arrays.c
+++ C_Learning/arrays.c
@@ -5,4 +5,12 @@
 int main(int argc, const char *argv)
 {
 
+      int array[10];
+
+      for(int i = 0; i <= 9; i++){
+        array[i] = i + 1;
+        printf("Number: %d\n", array[i]);
+      }
+
+      
 }
