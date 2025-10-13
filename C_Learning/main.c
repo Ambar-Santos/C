dd687c3430fcbeadd9589d1cad82a53f50c2f4c1
Ambar Santos <santosambar738@gmail.com>
2025-10-13 11:16:25 -0400

    Create main.c

---
+++ C_Learning/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+void explainVariables()
+{}
+
+int main(int argc, const char * argv[])
+{
+    explainVariables();
+    printf("Im the best \n");
+    
+    return 0; 
+} //Curly braces code are blocks 
+
+//Statement : anything that ends with a semicolon
