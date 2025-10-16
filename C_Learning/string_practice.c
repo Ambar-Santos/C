c514ed934b246262db511afe94046f9fde8c2098
Ambar Santos <santosambar738@gmail.com>
2025-10-16 18:56:40 -0400

    Create string_practice.c

---
+++ C_Learning/string_practice.c
@@ -0,0 +1,24 @@
+//String lesson learn-c org
+//Ambar Santos 
+//October 16th 2025
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+
+    char name[100] = "";
+    printf("Type your name: \n");
+    scanf("%99s", name);
+   
+
+
+    printf("So, your name is %s, it actually has %zu letters!\n", name, strlen(name));
+
+
+    getchar();
+
+    return 0;
+}
