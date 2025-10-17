9cb7e88b73d42b476c649fe21d2e63f73966b194
Ambar Santos <santosambar738@gmail.com>
2025-10-16 20:49:57 -0400

    Create string_practice2.c

---
+++ C_Learning/string_practice2.c
@@ -0,0 +1,17 @@
+//Secret name program
+/* This programs has a hardcoded secret name 
+the users type their name to see if it is the same 
+as the secret name, and if it is, it unlocks a secret
+message to all the people with that name*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+   const char * secret_name = "David";
+
+   char name[100] = "";
+
+
+
+}
